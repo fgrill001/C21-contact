b68fd23df18b0c7b8fefbd3ef2f43df43782ed3c
fgrill001 <f.grill@gmail.com>
2025-08-22 17:48:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,22 @@
-const CACHE_NAME = "c21-card-cache-v1";
+const CACHE_NAME = "c21-card-cache-v3";
 const URLS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png",
+  "./apple-touch-icon.png",
   "./fredrik-grill.vcf",
   "./fredrik-card.pdf"
 ];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS)));
 });
-
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
 });
-
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request).then((response) => {
-        // Optionally add new responses to cache
-        return response;
-      });
-    })
-  );
+  event.respondWith(caches.match(event.request).then((resp) => resp || fetch(event.request)));
 });
