b95689aa86975e411d5adef2173ca646bb637d0f
fgrill001 <f.grill@gmail.com>
2025-08-25 11:55:09 +0100

    Add files via upload

--- companion/sw.js
+++ companion/sw.js
@@ -1,8 +1,6 @@
 
-const CACHE="c21-agent-companion-v2";
+const CACHE="c21-agent-companion-v3-fixed";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-apple.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
