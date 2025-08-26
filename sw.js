c8bf0c03fcd892e53c87789c2bcac98d1e427a2e
fgrill001 <f.grill@gmail.com>
2025-08-26 12:34:41 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
 
-const CACHE_NAME="c21-card-cache-final";
-const URLS=["./","./index.html","./manifest.webmanifest",
-"./icon-192.png","./icon-512.png","./icon-apple.png",
-"./fredrik-grill.vcf","./fredrik-card.pdf"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
+const CACHE="c21-client-pwa-v-minimalicon";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./fredrik-grill.vcf","./fredrik-card.pdf"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
