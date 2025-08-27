616cf3c6f3c3e2cf2e7db5a2e382ac2cf2ff116c
fgrill001 <f.grill@gmail.com>
2025-08-27 12:12:07 +0100

    Add files via upload

--- companion/sw.js
+++ companion/sw.js
@@ -1,6 +1,6 @@
 
-const CACHE="c21-consultant-companion-v4";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
+const CACHE="c21-consultant-companion-v5";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./data/consultants.json","./data/fredrik.json","./avatars/fredrik.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
