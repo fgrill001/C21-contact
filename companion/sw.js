0fa05ed07ead77e393c4e6381b13987990abb4ac
fgrill001 <f.grill@gmail.com>
2025-09-03 15:35:43 +0100

    Update sw.js

--- companion/sw.js
+++ companion/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "c21-companion-v17";
+const CACHE = "c21-companion-v18";
 const ASSETS = [
   "./","./index.html","./manifest.webmanifest",
   "./icon-192.png","./icon-512.png","./apple-touch-icon.png",
