bbf9f05e2f447193bec969c981276d3ab73b0a2c
fgrill001 <f.grill@gmail.com>
2025-09-03 11:31:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "c21-client-v9";
+const CACHE = "c21-client-v11";
 const ASSETS = [
   "./","./index.html","./manifest.webmanifest",
   "./icon-192.png","./icon-512.png","./apple-touch-icon.png",
